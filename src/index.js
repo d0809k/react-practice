8d4b4abd7efda1c422db64ed007cdfc464907e17
DevK <101039503+d0809k@users.noreply.github.com>
2022-08-30 00:55:28 +0900

    Add CSS

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />); // app.js를 임포트하고 불러온다
